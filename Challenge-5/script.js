13fe21b79340ec55603429836b31474af626f8ee
Vinaya Sonawane <vinayasonawane91@gmail.com>
2025-06-14 20:13:35 +0530

    challenge - Function

--- Challenge-5/script.js
+++ Challenge-5/script.js
@@ -1,7 +1,24 @@
-const bill = 275;
-const tip = bill >= 50 && bill <= 300 ? (bill * 15) / 100 : (bill * 20) / 100;
-const totalValue = bill + tip;
+const calcAverage = (a, b, c) => (a + b + c) / 3;
+console.log(calcAverage(3, 7, 9));
 
-console.log(
-  `The bill was ${bill}, the tip was ${tip}, and the total value ${totalValue}`
-);
+let scoreDolphins = calcAverage(44, 23, 71);
+let scoreKoalas = calcAverage(65, 54, 49);
+
+console.log("dolphin score " + scoreDolphins, "koalas score " + scoreKoalas);
+
+const checkWinner = (avgDolphins, avgKoalas) => {
+  if (avgDolphins >= 2 * avgKoalas) {
+    console.log(`Dolphin win ${avgDolphins} vs ${avgKoalas}`);
+  } else if (avgKoalas >= 2 * avgDolphins) {
+    console.log(`Koalas win ${avgKoalas} vs ${avgDolphins}`);
+  } else {
+    console.log("No team wins...");
+  }
+};
+
+checkWinner(scoreDolphins, scoreKoalas);
+
+scoreDolphins = calcAverage(85, 54, 41);
+scoreKoalas = calcAverage(23, 34, 27);
+
+checkWinner(scoreDolphins, scoreKoalas);
