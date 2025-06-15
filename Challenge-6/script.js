725ce90b7714f3b6d6061d797b79dc3b1a004cca
Vinaya Sonawane <vinayasonawane91@gmail.com>
2025-06-15 15:40:21 +0530

    array - challange

---
+++ Challenge-6/script.js
@@ -0,0 +1,14 @@
+const calcTip = (bill) => {
+  const tip = bill >= 50 && bill <= 300 ? (bill * 15) / 100 : (bill * 20) / 100;
+  return tip;
+};
+
+console.log(calcTip(100));
+
+const bills = [125, 555, 44];
+
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+console.log(tips);
+
+const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+console.log(totals);
